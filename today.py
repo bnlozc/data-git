460d7937c1612eafa71eee6d4a9ac0628d7d4a98
Benal <benal.clk@gmail.com>
2026-04-26 17:53:45 +0300

    Fix style issues with docstrings

--- today.py
+++ today.py
@@ -1,8 +1,9 @@
-'''
-This is the module docstring
-'''
+"""Module for returning GitHub usernames."""
+
 def my_name_is():
+    """Return my GitHub username."""
     return "bnlozc"
 
 def my_buddy_is():
+    """Return my buddy's GitHub username."""
     return "bnlozc"
